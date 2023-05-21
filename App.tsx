2f7a3b84d53343dab466ef4a7289132261fe8539
luizroddev <luizwippe@icloud.com>
2023-05-21 14:07:08 -0300

    fix: removed unnecessary files and screens

--- App.tsx
+++ App.tsx
@@ -5,7 +5,6 @@ import HomeScreen from './src/screens/HomeScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
 import VisitorScreen from './src/screens/VisitorScreen'
 import LoginScreen from './src/screens/LoginScreen';
-import ChatScreen from './src/screens/ChatScreen';
 
 type RootStackParamList = {
     Home: undefined;
@@ -25,7 +24,6 @@ export default function App() {
                 <Stack.Screen name="Register" component={RegisterScreen} />
                 <Stack.Screen name="Visitor" component={VisitorScreen} />
                 <Stack.Screen name="Login" component={LoginScreen} />
-                <Stack.Screen name="Chat" component={ChatScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
